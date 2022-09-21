84a18dc7487fe65f7229377d799c9366ef023ac1
William05Cardozo <3784@holbertonschool.com>
2022-09-20 21:04:00 -0500

    PROJECT: 0x01: Python Async, Task 4 Correction

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python3
 """
-Take the code from wait_n and alter it into a new function task_wait_n
+Take the code from wait_n.
 """
-
 from typing import List
 import asyncio
-import random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """ Waits for ran delay until max_delay, returns list of actual delays """
-    spawn_list = []
-    delay_list = []
+async def task_wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
+    """call funtion task_wait_n"""
+    lista: List[float] = []
+    order_list: List[float] = []
     for i in range(n):
-        delayed_task = task_wait_random(max_delay)
-        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
-        spawn_list.append(delayed_task)
-
-    for spawn in spawn_list:
-        await spawn
-
-    return delay_list
+        lista.append(task_wait_random(max_delay))
+    for x in asyncio.as_completed(lista):
+        get_number = await x
+        order_list.append(get_number)
+    return order_list
