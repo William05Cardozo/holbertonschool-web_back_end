8cc7b8cb7c0476378b4c683873284722a7fa6358
William05Cardozo <3784@holbertonschool.com>
2022-09-30 22:13:55 -0500

    PROJECT_Interview: 0x05: Personal Data, Task 0, README

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
